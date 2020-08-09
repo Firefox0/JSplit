28d343753ea79c11f6be2f52a58a588f062bf7f0
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-09 11:19:10 +0200

    table to dict function

--- electron/app.js
+++ electron/app.js
@@ -179,93 +179,6 @@ class Timer {
 
         this.pause_time = new Date().getTime();
     }
-
-    add_split(row_index = -1) {
-        this.split_button.disabled = false;
-
-        let content = this.user_input.value;
-        if (content) {
-            // add row and keep count of splits
-            let row = this.splits.insertRow(row_index);
-            row.onclick = (() => this.select_row(row)).bind(this);
-            let split_name = row.insertCell(SPLIT_NAME);
-            split_name.innerText = content;
-            // create cells for current time, comparison and previous time
-            for (let i = 1; i <= 4; i++) {
-                let new_row = row.insertCell(i);
-                new_row.style.textAlign = "right"
-                new_row.innerText = "/";
-            }
-            // clear form
-            this.user_input.value = "";
-        }
-        // focus after add was pressed
-        this.user_input.focus();
-        this.set_transparent_background();
-    }
-    
-    set_game() {
-        if (this.current_game.style.visibility == "hidden") {
-            this.current_game.style.visibility = "";
-        }
-        this.current_game.innerText = this.user_input.value; 
-        this.user_input.value = "";
-        this.user_input.focus();
-    }
-    
-    reset() {
-        this.stop_button.disabled = true;
-        this.pause_button.disabled = true;
-        this.delete_button.disabled = true;
-        this.insert_above_button.disabled = true;
-        this.insert_below_button.disabled = true;
-        this.reset_button.disabled = true;
-        this.start_button.disabled = false;
-        this.append_button.disabled = false;
-        
-        this.user_input.value = "";
-        this.timer_time.innerText = "00:00:00.000";
-        this.delete_splits();
-        
-        this.timer_running = false;
-        this.start_time = null;
-        this.current_row = 0;
-        this.splits_selected = 0;
-
-        if (this.current_game.style.visibility != "hidden") {
-            this.current_game.style.visibility = "hidden";
-        }
-    }
-    
-    delete_split() {
-        for (let i = 0; i < this.splits.rows.length; i++) {
-            if (this.splits.rows[i].style.color == "black") {
-                this.splits.deleteRow(i);
-                this.splits_selected--;
-                // decrement to make sure that you iterate through all elements (otherwise skip after deletion)
-                i--;
-            }
-        }
-        this.delete_button.disabled = true;
-        // set transparent background when no splits were created (prevent black spot)
-        if (!this.splits_exist()) {
-            this.splits.style.background = "transparent";
-        }
-        if (!this.splits_selected) {
-            this.insert_above_button.disabled = true;
-            this.insert_below_button.disabled = true;
-        }
-    }
-    
-    insert_split(offset = 0) {
-        for (let i = 0; i < this.splits.rows.length; i++) {
-            if (this.splits.rows[i].style.color == "black") {
-            this.add_split(i + offset);
-            this.reset_splits_color();
-            return;
-            }
-        }
-    }
     
     change_background(image_path) {
         if (image_path) {
@@ -298,7 +211,7 @@ class Timer {
         // prevent black spot
         this.splits.style.background = "transparent";
     }
-    
+
     move_current_times() {
         if (this.run_completed()) {
             // move the current times to the previous times
@@ -331,34 +244,6 @@ class Timer {
             }
         }
     }
-
-    select_row(e) {
-        // default color is empty even though it looks white
-        if (e.style.color != "black") {
-            e.style.color = "black";
-            this.splits_selected++;
-        }
-        else {
-            e.style.color = "";
-            this.splits_selected--;
-        }
-        if (this.splits_selected > 0) {
-            if (!this.timer_running) {
-                if (this.splits_selected == 1) {
-                    this.insert_above_button.disabled = false;
-                    this.insert_below_button.disabled = false;
-                }
-                else {
-                    this.insert_above_button.disabled = true;
-                    this.insert_below_button.disabled = true;
-                }   
-            }
-            this.delete_button.disabled = false;
-        }
-        else {
-            this.delete_button.disabled = true;
-        }
-    }
     
     toggle_element(element) {
         elemen.style.visibility = element.style.visibility == "hidden" ? "" : "hidden";
@@ -370,7 +255,6 @@ class Timer {
             this.toggle_element(this.split_elements[i]);
         }
     }
-    
     
     request_save_split() {
         // request to show dialog
@@ -492,43 +376,17 @@ class Timer {
         this.pause_button.onclick = this.pause_timer.bind(this);
         this.pause_button.disabled = true;
 
-        this.splits = document.getElementById("table");
-        this.splits.style.background = "transparent";
-
-        this.user_input = document.getElementById("user-input");
-
-        this.append_button = document.getElementById("append-button");
-        this.append_button.onclick = (() => this.add_split()).bind(this);
-
         this.split_button = document.getElementById("split-button");
         this.split_button.onclick = this.split.bind(this);
         this.split_button.disabled = true;
 
-        this.delete_button = document.getElementById("delete-button");
-        this.delete_button.onclick = this.delete_split.bind(this);
-        this.delete_button.disabled = true;
+        this.background = document.getElementById("background-image");
 
-        this.insert_above_button = document.getElementById("insert-above-button");
-        this.insert_above_button.onclick = this.insert_split.bind(this);
-        this.insert_above_button.disabled = true;
-
-        this.insert_below_button = document.getElementById("insert-below-button");
-        this.insert_below_button.onclick = (() => this.insert_split(1)).bind(this);
-        this.insert_below_button.disabled = true;
-
-        this.reset_button = document.getElementById("reset-button");
-        this.reset_button.onclick = this.reset.bind(this);
-        this.reset_button.disabled = true;
-
-        this.split_elements = document.getElementsByClassName("split-buttons");
-
+        this.splits = document.getElementById("table");
+        this.splits.style.background = "transparent";
+        
         this.current_game = document.getElementById("game");
         this.current_game.style.visibility = "hidden";
-
-        this.set_game_button = document.getElementById("set-game-button");
-        this.set_game_button.onclick = this.set_game.bind(this);
-
-        this.background = document.getElementById("background-image");
 
         this.start_ipc();
         this.key_listener();
